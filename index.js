3cc6bd40b1c87a07437f9c34e0c85dd38dc2a80e
adnanyangilic <adnanyangilic@yahoo.com>
2024-09-26 21:30:36 +0300

    Proje dosyası ekle.

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+async function initMap() {
+    // Request needed libraries.
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    const map = new Map(document.getElementById("map"), {
+      center: { lat: 37.39094933041195, lng: 37.02503913145092 },
+      zoom: 10,
+      mapId: "4504f8b37365c3d0",
+    });
+    const infoWindow = new InfoWindow();
+    const draggableMarker = new AdvancedMarkerElement({
+      map,
+      position: { lat: 37.39094933041195, lng: 37.02503913145092 },
+      gmpDraggable: true,
+      title: "This marker is draggable.",
+    });
+  
+    draggableMarker.addListener("dragend", (event) => {
+      const position = draggableMarker.position;
+       document.querySelector("#val1").value=`${Math.round(position.lat*1000)/1000}`;
+       document.querySelector("#val2").value=`${Math.round(position.lng*1000)/1000}`;
+      infoWindow.close();
+      infoWindow.setContent(`Pin dropped at: ${position.lat}, ${position.lng}`);
+      infoWindow.open(draggableMarker.map, draggableMarker);
+    });
+
+  }
+  
+  initMap();
+  
---
+++ myJs.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+file.onchange = function(e) {
+  for (let i = 0; i < this.files.length; i++) {
+    let url = URL.createObjectURL(this.files[i]);
+    let img = new Image();
+    img.src = url;
+    img.setAttribute("style", "max-width:100px;");
+    img.setAttribute("class", "imgClass");
+    document.querySelector('#panel').appendChild(img);
+    // you can even free these 10bits you occupy in memory if you don't need the url anymore
+    img.onload = function() {
+      URL.revokeObjectURL(this.src);
+    }
+    console.log(this.files[i].name);
+  }
+}
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#buttiny').addEventListener('click', function() {
+
+    document.querySelector("#val3").value=document.querySelector("#val1").value;
+    document.querySelector("#val4").value=document.querySelector("#val2").value;
+
+    console.log("buttiny clicked");
+
+
+  });
